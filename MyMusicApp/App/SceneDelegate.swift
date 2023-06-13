597a0a205fa410388c5403f0e26918f24332516c
Alexey Davidenko <alexeydavidenko@icloud.com>
2023-06-13 20:00:58 +0200

    add tabbar, folders

--- MyMusicApp/App/SceneDelegate.swift
+++ MyMusicApp/App/SceneDelegate.swift
@@ -8,17 +8,21 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootVC = OnboardingViewController()
+        
+        let tabBarViewController = TabBarViewController()
+        
+        let rootVC = HomeViewController()
         let navigationController = UINavigationController(rootViewController: rootVC)
-        window?.rootViewController = navigationController
+        
+        window?.rootViewController = tabBarViewController.createTabBar()
+        //window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
-
