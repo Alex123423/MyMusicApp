0c9aab1ade92bf49b9974800893206251d4ad8b0
«Daniil <danilkulikovskiy@gmail.com>
2023-06-20 15:22:12 +0300

    added MVC

--- MyMusicApp/App/SceneDelegate.swift
+++ MyMusicApp/App/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let songVC = SongPlayer()
+        let songVC = SongPlayerViewController()
         
         let rootVC = SignInViewController()
         let navigationController = NavBarController(rootViewController: songVC)
