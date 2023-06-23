53508c4d1118e577de1a06760f95e35718d4b31a
Alexey Davidenko <alexeydavidenko@icloud.com>
2023-06-23 16:45:16 +0200

    change rootVC

--- MyMusicApp/App/SceneDelegate.swift
+++ MyMusicApp/App/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        //let rootVC = SignInViewController()
-        let rootVC = AlbumViewController()
+        let rootVC = SignInViewController()
         let navigationController = NavBarController(rootViewController: rootVC)
         window?.rootViewController = navigationController
 //        window?.rootViewController = SettingsViewController()
