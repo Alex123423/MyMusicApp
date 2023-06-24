4d28b8e85eca14aaff64a50edaa0b8d20d364eb6
Ojidaemo <vit.martsinovich@gmail.com>
2023-06-24 18:24:46 +0300

    Add notifications

--- MyMusicApp/Screen/Player/SongMain/SongPlayerViewController.swift
+++ MyMusicApp/Screen/Player/SongMain/SongPlayerViewController.swift
@@ -167,19 +167,17 @@ final class SongPlayerViewController: UIViewController {
             print("Failed to download track sample.")
         }
     }
-}
-
+    
     @objc func touchSlider() {
-        guard let player = player else { return }
         let time = CMTime(seconds: Double(songPlayer.progressBar.value), preferredTimescale: 1000)
         player.seek(to: time) { _ in
         }
     }
-
-@objc func shuffleTracks() {
-    print("Shuffle track")
-}
-
+    
+    @objc func shuffleTracks() {
+        print("Shuffle track")
+    }
+    
     func configureSongPlayerView(sender: Album) {
         songPlayer.artistTitle.text = sender.artistName
         songPlayer.songTitle.text = sender.trackName
@@ -200,7 +198,7 @@ final class SongPlayerViewController: UIViewController {
         let updatedSymbol = pauseSymbol!.withConfiguration(symbolConfiguration)
         songPlayer.playTrack.setImage(updatedSymbol, for: .normal)
     }
-
+    
     @objc func playPause() {
         guard let url = URL(string: prewiewUrlTrack) else { return }
         let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 32, weight: .regular)
@@ -212,9 +210,6 @@ final class SongPlayerViewController: UIViewController {
             songPlayer.playTrack.setImage(updatedSymbol, for: .normal)
             print("track time - \(Float(player.currentItem?.asset.duration.seconds ?? 0))")
             songPlayer.progressBar.maximumValue = Float(player.currentItem?.asset.duration.seconds ?? 0)
-            //            songPlayer.progressBar.minimumValue = Float(player.currentItem?.duration.seconds ?? 0)
-            //            updateTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateProgressBar), userInfo: nil, repeats: true)
-            //            songPlayer.progressBar.maximumValue = Float(player.currentItem?.duration.seconds ?? 0)
             bigImageView()
         } else if player.timeControlStatus == .playing {
             player.pause()
@@ -224,45 +219,43 @@ final class SongPlayerViewController: UIViewController {
             smallImageView()
         }
     }
-
+    
     @objc func updateProgressBar() {
-        guard let player = player else { return }
         let currentTime = player.currentTime().seconds
         let duration = player.currentItem?.duration.seconds ?? 0
         songPlayer.progressBar.value = Float(currentTime)
         songPlayer.progressBar.maximumValue = Float(duration)
     }
-
-
+    
+    
     @objc func previousTrack() {
         let cell = delegate?.moveBackForPreviewsTrack()
         guard let cellCheck = cell else { return }
         configureSongPlayerView(sender: cellCheck)
     }
-
+    
     @objc func nextTrack() {
         let cell = delegate?.moveForwardForPreviewsTrack()
         guard let cellCheck = cell else { return }
         configureSongPlayerView(sender: cellCheck)
     }
-
-@objc func repeatTrack() {
-    print("Repeat Song")
-}
-
+    
+    @objc func repeatTrack() {
+        print("Repeat Song")
+    }
+    
     @objc func monitorPlayerTime() {
-            guard let currentItem = player.currentItem else { return }
-            
-            let duration = currentItem.asset.duration.seconds
-            let currentTime = currentItem.currentTime().seconds
-            songPlayer.progressBar.maximumValue = Float(duration)
-            songPlayer.progressBar.value = Float(currentTime)
-            
-            let formattedCurrentTime = formatTime(currentTime)
-            let formattedDuration = formatTime(duration)
-            songPlayer.startSongTimer.text = formattedCurrentTime
-            songPlayer.endSongTimer.text = formattedDuration
+        guard let currentItem = player.currentItem else { return }
         
+        let duration = currentItem.asset.duration.seconds
+        let currentTime = currentItem.currentTime().seconds
+        songPlayer.progressBar.maximumValue = Float(duration)
+        songPlayer.progressBar.value = Float(currentTime)
+        
+        let formattedCurrentTime = formatTime(currentTime)
+        let formattedDuration = formatTime(duration)
+        songPlayer.startSongTimer.text = formattedCurrentTime
+        songPlayer.endSongTimer.text = formattedDuration
     }
     
     //MARK: - Time Setup
@@ -274,9 +267,9 @@ final class SongPlayerViewController: UIViewController {
             self?.bigImageView()
         }
     }
-
     
-    func asyncscheduledTimer(){
+    
+    func asyncscheduledTimer() {
         DispatchQueue.main.async {
             Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] timer in
                 self?.monitorPlayerTime()
@@ -290,7 +283,7 @@ final class SongPlayerViewController: UIViewController {
         let seconds = Int(time) % 60
         return String(format: "%02d:%02d", minutes, seconds)
     }
-
+    
     //MARK: - Animations
     func bigImageView() {
         UIView.animate(withDuration: 1,
@@ -313,5 +306,4 @@ final class SongPlayerViewController: UIViewController {
             self.songPlayer.pictureSong.transform = .identity
         }
     }
-    
 }
